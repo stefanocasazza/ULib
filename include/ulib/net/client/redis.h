51a7e4162a52fbeab827f5a7b6e51ca1c99adb7f
Victor Stewart <vdog5@mac.com>
2019-09-05 13:19:09 -0400

    Update redis.h

--- include/ulib/net/client/redis.h
+++ include/ulib/net/client/redis.h
@@ -1003,7 +1003,7 @@ struct RedisClusterNode {
 
    RedisClusterNode(const UString& _ipAddress, uint16_t _port, uint16_t _lowHashSlot, uint16_t _highHashSlot, UREDISClusterMaster *master) : ipAddress(_ipAddress), client(master), port(_port), lowHashSlot(_lowHashSlot), highHashSlot(_highHashSlot)
    {
-      client.connect(ipAddress.data(), port);
+      client.connect(ipAddress.c_str(), port);
    }
 
 #if defined(U_STDCPP_ENABLE) && defined(DEBUG)
